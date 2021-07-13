bbde6d8345249a373712c5c3ee3021ac8afc9373
Vincent Tsai <vincentvtt@hotmail.com>
2021-07-13 15:01:22 +1000

    feat: added bmi calculator route

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 app.use(express.urlencoded({ extended: false }))
 
+// Simple addition calculator on home page
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + "/index.html");
 });
@@ -11,6 +13,19 @@ app.post('/', (req, res) => {
     const num2 = Number(req.body.num2);
     const result = num1 + num2
     res.send("Sum of the two numbers is " + result);
+})
+
+// BMI Calculator on bmicalculator route
+
+app.get('/bmicalculator', (req, res) => {
+    res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+app.post('/bmicalculator', (req, res) => {
+    const weight = parseFloat(req.body.weight);
+    const height = parseFloat(req.body.height);
+    const result = (weight/(height * height)).toFixed(2);
+    res.send("Your BMI is " + result);
 })
 
 app.listen(3000, () => {
